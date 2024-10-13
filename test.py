913f5acee055061bede2ad6c287ed9a35b2b8ce9
alexakimenko <alexandr.akimenko@gmail.com>
2024-10-13 19:45:17 +1100

    add test_get_score and test_get_interests

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import datetime
 import functools
 import hashlib
+import json
 import unittest
 from unittest.mock import patch
 import fakeredis
 
 import api  # предполагается, что api.py содержит метод method_handler
 from store import Store
+from scoring import get_score, get_interests
 
 
 def cases(cases):
@@ -226,6 +228,40 @@ class TestSuite(unittest.TestCase):
         response, code = self.get_response(request)
         self.assertEqual(api.OK, code, (arguments, response))
         self.assertEqual(len(arguments["client_ids"]), len(response))
+
+
+    @cases(
+        [
+            ({"phone": "79175002040", "email": "stupnikov@otus.ru"}, 3.0),
+            ({
+                "phone": "79175002040",
+                "email": "stupnikov@otus.ru",
+                "gender": 1,
+                "birthday": datetime.date(2000, 1, 1),
+                "first_name": "a",
+                "last_name": "b",
+            }, 5.0),
+            ({
+                 "phone": "79175002040",
+                 "email": "stupnikov@otus.ru",
+                 "gender": 1,
+                 "first_name": "a",
+             }, 3.0),
+        ]
+    )
+    def test_get_score(self, data, expected):
+        assert get_score(self.store, **data) == expected
+
+    @cases(
+        [
+            ({"cid": "1"}, ["cars", "pets"]),
+            ({"cid": "2"}, ["cars", "pets", "travel"]),
+        ]
+    )
+    def test_get_interests(self, data, expected):
+        cid = data.get('cid')
+        self.store.cache_set(f"i:{cid}", json.dumps(expected), 60 * 60)
+        self.assertEqual(get_interests(self.store, cid), expected)
 
 
 if __name__ == "__main__":
